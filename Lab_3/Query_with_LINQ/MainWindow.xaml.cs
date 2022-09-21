e6fe73432670ca05627e06479d8cfbe1048dff7e
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-09-21 13:47:25 -0700

    Filled in the blanks

--- Lab_3/Query_with_LINQ/MainWindow.xaml.cs
+++ Lab_3/Query_with_LINQ/MainWindow.xaml.cs
@@ -28,60 +28,60 @@ namespace wpfbook01
 
             // get authors and ISBNs of each book they co-authored
             var authorsAndISBNs =
-               from author in dbcontext._______________
-               from book in author.____________________
-               orderby author.LastName, _____________________
-               select new { author.FirstName, author.LastName, ______________ };
+               from author in dbcontext.Authors
+               from book in author.Titles
+               orderby author.LastName, book.ISBN
+               select new { author.FirstName, author.LastName, book.ISBN };
 
             outputTextBox.AppendText("Authors and ISBNs:");
 
             // display authors and ISBNs in tabular format
-            foreach (var element in __________________)
+            foreach (var element in authorsAndISBNs)
             {
-                outputTextBox.AppendText($"\r\n\t{element.__________________,-10} " +
-                   $"{element.__________,-10} {element.ISBN,-10}");
+                outputTextBox.AppendText($"\r\n\t{element.FirstName,-10} " +
+                   $"{element.LastName,-10} {element.ISBN,-10}");
             }
 
             // get authors and titles of each book they co-authored
             var authorsAndTitles =
-               from book in _______________
-               from author in __________________
-               orderby author.LastName, author.FirstName, _______________
-               select new { author.FirstName, author.LastName, _____________ };
+               from book in dbcontext.Titles
+               from author in book.Authors
+               orderby author.LastName, author.FirstName, book.Title1
+               select new { author.FirstName, author.LastName, book.Title1 };
 
             outputTextBox.AppendText("\r\n\r\nAuthors and titles:");
 
             // display authors and titles in tabular format
             foreach (var element in authorsAndTitles)
             {
-                outputTextBox.AppendText($"\r\n\t{element.__________,-10} " +
-                   $"{element.___________,-10} {element.____________}");
+                outputTextBox.AppendText($"\r\n\t{element.FirstName,-10} " +
+                   $"{element.LastName,-10} {element.Title1}");
             }
 
             // get authors and titles of each book 
             // they co-authored; group by author
             var titlesByAuthor =
-               from author in dbcontext.______________
+               from author in dbcontext.Authors
                orderby author.LastName, author.FirstName
                select new
                {
-                   Name = author.FirstName + " " +____________________,
+                   Name = author.FirstName + " " + author.LastName,
                    Titles =
-                     from book in _______________
-                     orderby book._____________
-                     select book.________________
+                     from book in author.Titles
+                     orderby book.Title1
+                     select book.Title1
                };
 
             outputTextBox.AppendText("\r\n\r\nTitles grouped by author:");
 
             // display titles written by each author, grouped by author
-            foreach (var author in _________________)
+            foreach (var author in titlesByAuthor)
             {
                 // display author's name
-                outputTextBox.AppendText($"\r\n\t{______________}:");
+                outputTextBox.AppendText($"\r\n\t{author.Name}:");
 
                 // display titles written by that author
-                foreach (var title in ______________)
+                foreach (var title in author.Titles)
                 {
                     outputTextBox.AppendText($"\r\n\t\t{title}");
                 }
