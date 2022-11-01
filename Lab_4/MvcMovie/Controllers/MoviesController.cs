631ddcd3a8b9c41195dd10e9b6c8a63568283b94
huynnguyen <huynnguyen00@gmail.com>
2022-10-31 23:30:57 -0700

    Part 7 changes

--- Lab_4/MvcMovie/Controllers/MoviesController.cs
+++ Lab_4/MvcMovie/Controllers/MoviesController.cs
@@ -22,7 +22,15 @@ namespace MvcMovie.Controllers
         // GET: Movies
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Movie.ToListAsync());
+            var movies = from m in _context.Movie
+                 select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
