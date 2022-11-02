54d11eb644e0c9b046212c4655b14ac1e9936893
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-11-01 19:19:54 -0700

    fixed build errors

--- Lab_4/MvcMovie/Models/MovieGenreViewModel.cs
+++ Lab_4/MvcMovie/Models/MovieGenreViewModel.cs
@@ -1,3 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
