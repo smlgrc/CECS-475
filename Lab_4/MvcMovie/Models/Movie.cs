9c2f78cd04070291a6dfaa7e6ba34fdf0580263c
smlgrc <71049193+smlgrc@users.noreply.github.com>
2022-11-01 20:26:12 -0700

    Finished part 9

--- Lab_4/MvcMovie/Models/Movie.cs
+++ Lab_4/MvcMovie/Models/Movie.cs
@@ -13,7 +13,8 @@ namespace MvcMovie.Models
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        //[DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
         [Range(1, 100)]
